48de1ba2e7211a67ce5d0e4ceaeaccbd81dd80d7
tuswang <tuswang@tencent.com>
2021-09-14 08:20:45 +0800

    Chapter01-intro

--- APUE/src/intro/getcputc.c
+++ APUE/src/intro/getcputc.c
@@ -1,10 +1,15 @@
+/*
+ * @Description: input from stdin and output to stdout char by char
+ * @Version: 2.0
+ * @Autor: tusikalanse
+ * @Date: 2014-03-20 09:38:24
+ * @LastEditors: tusikalanse
+ * @LastEditTime: 2021-09-14 08:07:05
+ */
 #include "apue.h"
 
-int
-main(void)
-{
+int main(void) {
 	int		c;
-
 	while ((c = getc(stdin)) != EOF)
 		if (putc(c, stdout) == EOF)
 			err_sys("output error");
